d52008871f2a612aa8b484255ac810401bf92a3f
Samira Esquina <samiraesquina@gmail.com>
2019-08-03 10:04:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function formatQueryParams(params) {
 
 function displayResults(responseJson, maxResults) {
     console.log(responseJson);
-    
+
     $('.js-error').empty();
     $('.results-list').empty();
-  
+
     for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
         $('.results-list').append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
         <p>${responseJson.data[i].description}</p>
@@ -20,17 +20,17 @@ function displayResults(responseJson, maxResults) {
 }
 
 function getParks(baseUrl, stateArr, maxResults, apiKey) {
-   
+
     const params = {
         stateCode: stateArr,
         limit: maxResults
     }
-   
+
     const queryString = formatQueryParams(params);
     const url = baseUrl + '?' + queryString + '&api_key=' + apiKey;
     console.log(url);
-   
-    
+
+
     fetch(url)
     .then(response => {
         if (response.ok) {
@@ -51,8 +51,8 @@ function watchForm() {
         const baseUrl = 'https://api.nps.gov/api/v1/parks'
         const stateArr = $('.js-state-entered').val().split(",");
         const maxResults = $('.js-result-amt').val();
-        
-        const apiKey = 'UTz8fgK79uQu8UpWymKj7i5CiBfBcJSPmtvb33Hd'
+
+        const apiKey = 'wcjvPe7MBTEr5cDaJaly2SD9dRjK9t4s7YoG9L5g'
         getParks(baseUrl, stateArr, maxResults, apiKey);
     })
 }
